690673ee403f3c0e60178857cebe1c8192092365
christhoph <cristoph.solis@outlook.com>
2020-02-24 19:31:37 -0500

    First commit and setup

---
+++ src/containers/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { HomeContainer, HomeGreeting } from "./styles";
+
+const Home = () => (
+  <HomeContainer>
+    <HomeGreeting>Hello World!</HomeGreeting>
+  </HomeContainer>
+);
+
+export default Home;
