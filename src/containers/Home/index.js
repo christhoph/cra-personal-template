0490ea1225cae42524262d870913c2786ee1632d
christhoph <cristoph.solis@outlook.com>
2020-02-24 19:39:56 -0500

    Add Greetings components example

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import { HomeContainer, HomeGreeting } from "./styles";
+import { HomeContainer } from "./styles";
+import Greetings from "../../components/Greetings";
 
 const Home = () => (
   <HomeContainer>
-    <HomeGreeting>Hello World!</HomeGreeting>
+    <Greetings />
   </HomeContainer>
 );
 
--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -1,17 +1,9 @@
 import styled from "styled-components";
 
-import { convertHexToRgba, colors } from "../../core";
-
-const { primary } = colors;
-
 export const HomeContainer = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-export const HomeGreeting = styled.h2`
-  color: ${convertHexToRgba(primary, 0.8)};
 `;
